5e2f0ea4f46ce78107d136774eba358a851bf8d4
SOPHYS Thibaut <thibaut.sophys@etu.univ-nantes.fr>
2022-07-09 18:50:53 +0200

    Probably final push I'm done with this fuck it

--- end.js
+++ end.js
@@ -1,13 +1,13 @@
 const username = document.querySelector('#username');
 const saveScoreBtn = document.querySelector('#saveScore');
 let finalScore = document.querySelector('#finalScore');
-const mostRecentScore = document.querySelector('#mostRecentScore');*
+const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
 
-final.score.innerText = mostRecentScore
+finalScore.innerText = mostRecentScore
 
 username.addEventListener('keyup', () => {
   saveScore.disabled = !username.value
