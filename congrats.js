7af852f2465b3ce8f5441a1d46fd89d787d3247f
Grahaam <41477458+Grahaam@users.noreply.github.com>
2022-07-21 12:49:12 +0400

    Update congrats.js

--- congrats.js
+++ congrats.js
@@ -8,14 +8,14 @@ const sc = new ScratchCard
         containerWidth: scContainer.offsetWidth,
         containerHeight: 330,
         brushSrc: '',
-        imageForwardSrc: './images/scratch.png',
+        imageForwardSrc: './images/pngwing.com (2).png',
         imageBackgroundSrc: './images/istockphoto-1219614648-612x612.png',
         htmlBackground: ``,
         clearZoneRadius: 40,
         nPoints: 50,
         pointSize: 4,
         callback: () => {
-            scInfos.innerText = 'I am your Slut <3';
+            scInfos.innerText = 'TICKET TO BARCELONA (not the real ones yet)';
             const img = document.querySelectorAll(".sc__container img");
             img[0].style.filter = "blur(0)";
             img[0].style.animation = "pop 1.5s alternate ease-in-out infinite";
@@ -25,8 +25,6 @@ const sc = new ScratchCard
 // Init
 sc.init().then(() => {
   sc.canvas.addEventListener('scratch.move', () => {
-    let percent = sc.getPercent().toFixed(0);
-      scInfos.innerHTML = percent + '%';
   })
 }).catch((error) => {
   // image not loaded
